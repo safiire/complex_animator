c53739296f5e276a6a2b1ee4ed7e62e60fe82bdf
Safiire <safiire@gmail.com>
2016-05-25 22:50:27 -0700

    Animations are working

--- src/color/color.h
+++ src/color/color.h
@@ -3,4 +3,5 @@
 #include <complex>
 #include <cmath>
 
+
 glm::vec3 complex_to_hsv(std::complex<double> z);
--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,38 @@
 #include <gtx/color_space.hpp>
 #include <stdio.h>
 
+
 int main(){
+  using namespace std;
+  using namespace glm;
 
   ivec2 size(1024, 1024);
   ViewPort<double> view_port(size, 3.0, -3.0, 3.0, -3.0);
   NetPPM image = NetPPM(size);
 
-  image.map_each_pixel([&view_port] (const vec2 &pixel_coord) {
+  int fps = 30;
+  int total_time = 5;
+  int total_frames = total_time * fps;
 
-    complex<double> z = view_port.pixel_to_complex(pixel_coord);
-    z = z - 1.0 - std::pow(z, -1);
-    auto hsv = complex_to_hsv(z);
-    return glm::rgbColor(hsv) * vec3(255, 255, 255);
-  });
+  double t = 0.0;
+  double t_delta = pow(fps, -1);
+  char filename[256];
 
-  image.write_file("output.ppm");
+  for(int frame = 0; frame < total_frames; frame++){
+
+    image.map_each_pixel([&view_port, &t] (const vec2 &pixel_coord) {
+
+      complex<double> z = view_port.pixel_to_complex(pixel_coord);
+      //z = z - (1.0 * t) - std::pow(z, -1 * t);
+      z = sin(z * t) * cos(z * t) * pow(z, -1);
+      auto hsv = complex_to_hsv(z);
+      return glm::rgbColor(hsv) * vec3(255, 255, 255);
+    });
+
+    sprintf(filename, "out/output_%05d.ppm", frame);
+    image.write_file(filename);
+    t += t_delta;
+  }
 
   return 0;
 }
